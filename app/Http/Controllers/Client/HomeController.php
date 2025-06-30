797d4a5b49acfba0dd81af297ca68ce74aaa5384
minh-24082005 <minhnvph49879@gmail.com>
2025-06-30 22:12:04 +0700

    Tạm lưu thay đổi để pull code mới

---
+++ app/Http/Controllers/Client/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Http\Controllers\Client;
+use App\Http\Controllers\Controller;
+
+use App\Models\Product; 
+use App\Models\Category;
+class HomeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+     
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Admin\ProductController;
 use App\Http\Controllers\Admin\CategoryController;
 use Illuminate\Support\Facades\Route;
 
